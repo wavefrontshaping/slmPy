d27a46e2537c0d15658b85b884d129aa2153cdc8
Your Name <you@example.com>
2021-03-18 09:10:03 +0100

    change organization + add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+# from packagename.version import Version
+
+setuptools.setup(name='slmpy',
+                 version='0.1.0',
+                 description='Display images on a spatial light modulators.',
+                 long_description=open('README.md').read().strip(),
+                 author="Sebastien M. Popoff",
+                 author_email='sebastien.popoff@espci.psl.eu',
+                 url='https://github.com/wavefrontshaping/slmPy',
+                 py_modules=['slmpy.slmpy'],
+                 install_requires=['numpy', 'wxPython'],
+                 license='MIT License',
+                 zip_safe=False,
+                 keywords='numpy, python3, slm, wavefront shaping, display',
+                 classifiers=[''])
