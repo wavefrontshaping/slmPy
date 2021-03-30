fb57f6001ce1c0b179efd9c66cad68ed3aa1e34e
Your Name <you@example.com>
2021-03-30 10:29:43 +0200

    change resolution

--- rpi_server/install.sh
+++ rpi_server/install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+# connect via ssn and execute this lines
+# git clone --branch network https://github.com/wavefrontshaping/slmPy.git
+# cd slmPy && bash rpi_server/install.sh
 
 sudo apt-get install python3-pip -y
 
@@ -11,14 +14,10 @@ sudo python3 -m pip install wxPython
 # deactivate screen blanking
 sudo bash -c 'echo -e "\n[SeatDefaults]\nxserver-command=X -s 0 -dpms" >> /etc/lightdm/lightdm.conf'
 
-# clone and install the repo
-git clone --branch network https://github.com/wavefrontshaping/slmPy.git
-cd slmPy
+# the repo
 sudo python3 setup.py install 
 
-# mkdir -p /home/pi/.config/lxsession/LXDE-pi/autostart
-
-# copy the server script that listen to port 9999 
+# copy the server script that listens to port 9999 
 # and display the received masks on the SLM
 cp rpi_server/server.py /home/pi/
 # create a desktop file that will launch the server script on startup
@@ -28,6 +27,8 @@ cp rpi_server/SLM.desktop /home/pi/.config/autostart/
 cd ..
 rm -rf slmPy
 
+export DISPLAY=:0
+xrandr --output HDMI-1 --mode "800x600"
 sudo reboot
 
 #sudo bash -c 'echo "consoleblank=0" >> /boot/cmdline.txt'
